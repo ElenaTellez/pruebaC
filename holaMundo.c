8e8818377c0757b48922061cb5bd809b5cf73d85
Elena Tellez <metellezs@hotmail.com>
2016-09-22 10:51:23 +0200

    colores

--- holaMundo.c
+++ holaMundo.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <ncurses.h>
 
 int main(){
 
-printf ("Hola mundo ");
+initscr(); 
+start_color();
 
+assume_default_colors(COLOR_BLUE, COLOR_WHITE);
 
+init_pair(1,COLOR_RED, COLOR_WHITE);
+attron(COLOR_PAIR(1));
+printw ("Hola mundo ");
+getch();
+ednwin();
+return 0;
 }
